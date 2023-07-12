fdf975ea2bc9194da9ba812283d3f79915d1b490
dev2alert <68344264+dev2alert@users.noreply.github.com>
2023-07-12 18:20:47 +0300

    Update Context.tsx

--- src/common/app/Context.tsx
+++ src/common/app/Context.tsx
@@ -19,7 +19,7 @@ export function useApp(): AppContextValue {
     const value = useContext(AppContext);
 
     if (!value) {
-        throw new Error("useApp must be used within a ThemeProvider.");
+        throw new Error("useApp must be used within a AppProvider.");
     }
 
     return value;
